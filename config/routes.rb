7b293e5d6e260e321c030c8616fe421dfcfe026c
Fumiya-Soeno <61449986+Fumiya-Soeno@users.noreply.github.com>
2021-02-14 17:46:50 +0900

    タイポ修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: {
     registrations: 'users/registrations',
-    registrations: 'users/sessions'
+    sessions: 'users/sessions'
   }
   root 'articles#index'
 
