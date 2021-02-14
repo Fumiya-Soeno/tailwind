713773da34a6d270ea608a8413c2584be0aef2dd
Fumiya-Soeno <61449986+Fumiya-Soeno@users.noreply.github.com>
2021-02-14 14:36:46 +0900

    ユーザー登録機能

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -10,9 +10,14 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # end
 
   # POST /resource
-  # def create
-  #   super
-  # end
+  def create
+    @user = User.new(sign_up_params)
+    if @user.save
+      @profile = Profile.create(params[:user][:profile].permit(:nickname).merge(user_id: @user.id))
+      sign_in(:user, @user)
+      redirect_to root_path
+    end
+  end
 
   # GET /resource/edit
   # def edit
--- db/migrate/20210207140410_create_profiles.rb
+++ db/migrate/20210207140410_create_profiles.rb
@@ -1,7 +1,15 @@
 class CreateProfiles < ActiveRecord::Migration[6.0]
   def change
     create_table :profiles do |t|
-
+      t.references :user,          null: false, foreign_key: true
+      t.boolean    :email_publish, null: false, default: false
+      t.string     :nickname
+      t.string     :site
+      t.string     :company
+      t.string     :residence
+      t.text       :profile
+      t.string     :twitter
+      t.string     :facebook
       t.timestamps
     end
   end
