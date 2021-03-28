6e76b2b73ce4c76516188ab461e7ee159af9c2a2
Fumiya-Soeno <61449986+Fumiya-Soeno@users.noreply.github.com>
2021-03-28 19:16:02 +0900

    サイドバーエラー解消

--- config/routes.rb
+++ config/routes.rb
@@ -18,9 +18,10 @@ Rails.application.routes.draw do
       patch "draft_to_article"
     end
   end
-  articles = ["index","timeline","tags","milestone","calendarfeed"]
+
+  articles = ["timeline","tags","milestone","calendarfeed"]
   articles.each do |r|
-    get "articles/#{r}"
+    get r, to: "articles#" + r
   end
 
   questions = ["index","timeline","tags","question","news"]
@@ -33,7 +34,7 @@ Rails.application.routes.draw do
       post "follow"
     end
   end
-  
+
   get   "profiles/edit"
   patch "profiles/update"
   get   "profiles/account"
